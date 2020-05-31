0978d498ea610237ed323e244fad6f3886e6e350
Rafael Robles <gdl.villber@gmail.com>
2020-05-31 16:37:09 -0500

    cambios android

--- platforms/android/app/src/main/assets/www/js/scripts.js
+++ platforms/android/app/src/main/assets/www/js/scripts.js
@@ -37,3 +37,43 @@ window.fn.pushPage = function (page, anim) {
   }
 };
 
+// select the right Ad Id according to platform
+  var admobid = {};
+  if( /(android)/i.test(navigator.userAgent) ) { // for android & amazon-fireos
+    admobid = {
+      banner: 'ca-app-pub-0211823635398974/7027316897', // or DFP format "/6253334/dfp_example_ad"
+      interstitial: 'ca-app-pub-0211823635398974/7728370762'
+    };
+  } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) { // for ios
+    admobid = {
+      banner: 'ca-app-pub-0211823635398974/7027316897', // or DFP format "/6253334/dfp_example_ad"
+      interstitial: 'ca-app-pub-0211823635398974/7728370762'
+    };
+  };
+
+function initApp() {
+  if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
+
+  // this will create a banner on startup
+  AdMob.createBanner( {
+    adId: admobid.banner,
+    position: AdMob.AD_POSITION.BOTTOM_CENTER,
+    isTesting: true, // TODO: remove this line when release
+    overlap: false,
+    offsetTopBar: false,
+    bgColor: 'black'
+  } );
+
+  // this will load a full screen ad on startup
+  AdMob.prepareInterstitial({
+    adId: admobid.interstitial,
+    isTesting: true, // TODO: remove this line when release
+    autoShow: true
+  });
+}
+
+if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
+    document.addEventListener('deviceready', initApp, false);
+} else {
+    initApp();
+}
