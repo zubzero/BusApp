fb5dea3bb6e6cc3ae12118b7ea3fe150e5c200c4
Rafael Robles <gdl.villber@gmail.com>
2020-05-31 20:04:11 -0500

    Cambios sdk version

--- platforms/android/app/src/main/assets/www/js/scripts.js
+++ platforms/android/app/src/main/assets/www/js/scripts.js
@@ -1,3 +1,30 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+        document.addEventListener("backbutton", onBackKeyDown, false);
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        // var listeningElement = parentElement.querySelector('.listening');
+        // var receivedElement = parentElement.querySelector('.received');
+
+        // listeningElement.setAttribute('style', 'display:none;');
+        // receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
 document.addEventListener('init', function(event) {
   var page = event.target;
 
@@ -76,4 +103,9 @@ if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
     document.addEventListener('deviceready', initApp, false);
 } else {
     initApp();
-}
+};
+
+function onBackKeyDown(){
+    window.history.back();
+};
+app.initialize();
